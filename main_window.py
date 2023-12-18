2daf7b82edbd507a1a0acd5e5f060b1b3e1f8383
wardonne <Wardonne@yandex.ru>
2023-12-18 19:43:32 +0400

    GUI

--- main_window.py
+++ main_window.py
@@ -1,9 +1,8 @@
 import sys
-from PyQt5.QtWidgets import (
-    QMessageBox, QFileDialog, QWidget, QLabel, QApplication, QPushButton,
-    QToolTip, QVBoxLayout, QScrollArea, QDialog, QLineEdit
-)
-from PyQt5.QtGui import QFont, QIcon
+from PyQt5.QtWidgets import (QWidget, QLabel, QApplication, QPushButton, QVBoxLayout, QHBoxLayout,
+                             QScrollArea, QDialog, QLineEdit, QFileDialog)
+from PyQt5.QtGui import QFont, QIcon, QPalette, QColor
+from PyQt5.QtCore import Qt
 
 
 class ScrollLabel(QScrollArea):
@@ -22,63 +21,83 @@ class ScrollLabel(QScrollArea):
         self.label.setText(text)
 
 
-class ErrorMessageBox:
-    def __init__(self, parent: QWidget, text: str) -> None:
-        message_box = QMessageBox(parent)
-        message_box.setText(text)
-        ok_button = message_box.addButton(QMessageBox.Ok)
-        ok_button.setStyleSheet(
-            "background:#007BFF; border-radius: 5px; min-width: 100px; color: #fff;")
-        message_box.setStyleSheet('color: #333;')
-        message_box.exec_()
-
-
 class Example(QWidget):
-
     def __init__(self) -> None:
         super().__init__()
         self.initUI()
-        self.setStyleSheet(
-            "background:#fefefe; color: #333; font-weight:bold; border-radius: 5px;")
+        self.setStyleSheet("background:#fefefe; color: #333; font-weight:bold; border-radius: 5px;")
 
     def initUI(self) -> None:
-        QToolTip.setFont(QFont('SansSerif', 10))
-        self.setWindowTitle('LAB_3')
+        self.setWindowTitle('6212 - lab3')
         self.setWindowIcon(QIcon('home.png'))
 
-        self.button = QPushButton('Create CSV-Dataset', self)
-        self.button.clicked.connect(self.click_csv)  # Placeholder, replace with actual function
-        self.button.setStyleSheet(
-            "background:#007BFF; border-radius: 5px; min-width: 200px; min-height: 50px; color: #fff;")
+        self.button = self.create_button('Create CSV-Dataset', self.click_csv)
+        self.button_dataset = self.create_button('Create Dataset-Copy', self.click_dataset)
+        self.button_dataset2 = self.create_button('Create Dataset-Copy2', self.click_dataset_copy)
 
-        self.button_dataset = QPushButton('Create Dataset-Copy', self)
-        self.button_dataset.clicked.connect(self.click_dataset)
-        self.button_dataset.setStyleSheet(
-            "background:#007BFF; border-radius: 5px; min-width: 200px; min-height: 50px; color: #fff;")
+        self.button_rating1 = self.create_button('1', self.get_next_review_1, 200, 50)
+        self.button_rating2 = self.create_button('2', self.get_next_review_2, 200, 50)
+        self.button_rating3 = self.create_button('3', self.get_next_review_3, 200, 50)
+        self.button_rating4 = self.create_button('4', self.get_next_review_4, 200, 50)
+        self.button_rating5 = self.create_button('5', self.get_next_review_5, 200, 50)
 
         self.review = ScrollLabel(self)
-        self.review.setStyleSheet(
-            "background:#d9d4e7; color: #0e172c; border-radius: 5px;")
+        self.review.setStyleSheet("background:#DFDFDF; color: #0e172c; border-radius: 5px;")
 
-        layout = QVBoxLayout(self)
+        main_layout = QVBoxLayout(self)
 
-        layout.addWidget(self.button_dataset)
-        layout.addWidget(self.button)
-        layout.addWidget(self.review)
+        main_buttons_layout = QHBoxLayout()
+        main_buttons_layout.addWidget(self.button_dataset)
+        main_buttons_layout.addWidget(self.button)
+        main_buttons_layout.addWidget(self.button_dataset2)
 
-    def click_csv(self):
-        print("Create CSV-Dataset")
+        rating_layout = QHBoxLayout()
+        rating_layout.addWidget(self.button_rating1)
+        rating_layout.addWidget(self.button_rating2)
+        rating_layout.addWidget(self.button_rating3)
+        rating_layout.addWidget(self.button_rating4)
+        rating_layout.addWidget(self.button_rating5)
 
-    def click_dataset(self):
-        print("Create Dataset-Copy")
+        main_layout.addLayout(main_buttons_layout)
+        main_layout.addLayout(rating_layout)
+        main_layout.addWidget(self.review)
+
+        self.show()
+
+    def create_button(self, text, slot, width=200, height=50):
+        button = QPushButton(text, self)
+        button.clicked.connect(slot)
+        button.setStyleSheet(
+            f"background:#007BFF; border-radius: 5px; min-width: {width}px; min-height: {height}px; color: #fff;")
+        return button
+
+    def get_next_review_1(self) -> None:
+        pass
+
+    def get_next_review_2(self) -> None:
+        pass
+
+    def get_next_review_3(self) -> None:
+        pass
+
+    def get_next_review_4(self) -> None:
+        pass
+
+    def get_next_review_5(self) -> None:
+        pass
+
+    def click_csv(self) -> None:
+        pass
+
+    def click_dataset(self) -> None:
+        pass
+
+    def click_dataset_copy(self) -> None:
+        pass
 
 
-def application() -> None:
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
     sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    application()
